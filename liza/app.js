bdfd70460371f48971c72225f36ebe5b2529fe75
putriliza <putriliza05@gmail.com>
2022-03-31 09:38:44 +0700

    update struktur data

--- liza/app.js
+++ liza/app.js
@@ -1,12 +1,18 @@
 function getData() {
 
     firebase.database().ref('/test').once('value').then(function (snapshot) {
-        var float = snapshot.val().float;
-        var int = snapshot.val().int;
-        var string = snapshot.val().string;
+        var Distance = snapshot.val().Distance;
+        var IR_HS = snapshot.val().IR_HS;
+        var IR_OUT = snapshot.val().IR_OUT;
+        var People = snapshot.val().People;
+        var State = snapshot.val().State;
+        var Temperature = snapshot.val().Temperature;
     
-        document.getElementById("float").innerHTML = float;
-        document.getElementById("int").innerHTML = int;
-        document.getElementById("string").innerHTML = string;
+        document.getElementById("Distance").innerHTML = Distance;
+        document.getElementById("IR_HS").innerHTML = IR_HS;
+        document.getElementById("IR_OUT").innerHTML = IR_OUT;
+        document.getElementById("People").innerHTML = People;
+        document.getElementById("State").innerHTML = State;
+        document.getElementById("Temperature").innerHTML = Temperature;
     })
 }
