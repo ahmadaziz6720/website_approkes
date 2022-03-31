8cd41c9556a74ba38fc4c16232ccd1bd2c62a230
ahmadaziz6720 <a715210@gmail.com>
2022-03-31 11:16:59 +0700

    Merge branch 'main' of https://github.com/ahmadaziz6720/website_approkes

--- liza/app.js
+++ liza/app.js
@@ -12,19 +12,20 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 function getData(){
-    firebase.database().ref('/test').once('value').then(function (snapshot) {
-        var Distance = snapshot.val().Distance;
-        var IR_HS = snapshot.val().IR_HS;
-        var IR_OUT = snapshot.val().IR_OUT;
-        var People = snapshot.val().People;
-        var State = snapshot.val().State;
-        var Temperature = snapshot.val().Temperature;
-    
-        document.getElementById("Distance").innerHTML = Distance;
-        document.getElementById("IR_HS").innerHTML = IR_HS;
-        document.getElementById("IR_OUT").innerHTML = IR_OUT;
-        document.getElementById("People").innerHTML = People;
-        document.getElementById("State").innerHTML = State;
-        document.getElementById("Temperature").innerHTML = Temperature;
-    });
+    while(true){
+        firebase.database().ref('/test').once('value').then(function (snapshot) {
+            var Distance = snapshot.val().Distance;
+            var IR_HS = snapshot.val().IR_HS;
+            var IR_OUT = snapshot.val().IR_OUT;
+            var People = snapshot.val().People;
+            var State = snapshot.val().State;
+            var Temperature = snapshot.val().Temperature;
+        
+            document.getElementById("Distance").innerHTML = Distance;
+            document.getElementById("IR_HS").innerHTML = IR_HS;
+            document.getElementById("IR_OUT").innerHTML = IR_OUT;
+            document.getElementById("People").innerHTML = People;
+            document.getElementById("State").innerHTML = State;
+            document.getElementById("Temperature").innerHTML = Temperature;
+        });}
 }
